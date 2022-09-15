308a2cbb5836ba84336ef4c3581b21673ce4a9d7
KostyaBelyaev <bazaaa2008@gmail.com>
2022-09-15 10:53:29 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,21 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setSurname("Беляев");
+        student.setName("Константин");
+        student.setPatronymic("Сергеевич");
+        student.setGroup(13262);
+        student.setCreated(new Date());
+        String lc = System.lineSeparator();
+        System.out.println("Фамилия: " + student.getSurname() + lc +
+                            "Имя: " + student.getName() + lc +
+                            "Отчество: " + student.getPatronymic() + lc +
+                            "Группа: " + student.getGroup() + lc +
+                            "Дата поступления: " + student.getCreated());
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Student.java
@@ -0,0 +1,51 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class Student {
+    private String surname;
+    private String name;
+    private String patronymic;
+    private int group;
+    private Date created;
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public int getGroup() {
+        return group;
+    }
+
+    public void setGroup(int group) {
+        this.group = group;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+}
