911be1a025ef754d5ae27c473e0411a66f48c2ad
KostyaBelyaev <bazaaa2008@gmail.com>
2022-09-19 19:35:30 +0300

    5. Tracker - хранилище [#396 #345030]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,21 @@
 package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Item {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return id == item.id && Objects.equals(created, item.created) && Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(created, id, name);
+    }
 
     LocalDateTime created = LocalDateTime.now();
 
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,23 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        int count = 0;
+        Item[] rsl = new Item[size];
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (key.equals(item.getName())) {
+                rsl[count] = items[index];
+                count++;
+            }
+        }
+        return Arrays.copyOf(rsl, count);
     }
 
     public Item findById(int id) {
@@ -21,5 +40,30 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public static void main(String[] args) {
+        Tracker tracker = new Tracker();
+        Item first = new Item("First");
+        Item second = new Item("Second");
+        Item third = new Item("Third");
+        Item fourth = new Item("First");
+        tracker.add(first);
+        tracker.add(second);
+        tracker.add(third);
+        tracker.add(fourth);
+        Item[] result = tracker.findAll();
+        for (int i = 0; i < result.length; i++) {
+            Item it = result[i];
+            System.out.println(it.getName() + " " + it.getId());
+        }
+        System.out.println(tracker.size);
+        Item[] rsl = tracker.findByName("Third");
+        for (int i = 0; i < rsl.length; i++) {
+            Item it = rsl[i];
+            if (it != null) {
+                System.out.println(it.getName() + " " + it.getId());
+            }
+        }
     }
 }
