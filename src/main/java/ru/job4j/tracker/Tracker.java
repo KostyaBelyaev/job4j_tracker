0a094c43b9f9a2dfd8b6d537ca87bb14f4bf7cf2
KostyaBelyaev <bazaaa2008@gmail.com>
2022-09-20 21:31:16 +0300

    6. Метод замены заявки. Tracker.replace [#211748 #345100]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -56,6 +56,18 @@ public class Tracker {
             return false;
     }
 
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        if (index != -1) {
+            items[index] = null;
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
+            return true;
+        }
+        return false;
+    }
+
 }
 
 
